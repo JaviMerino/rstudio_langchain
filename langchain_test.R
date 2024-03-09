7de5897bbd55bbb4491907c2a2dc6ec7d88860dc
Javi Merino <merino.jav@gmail.com>
2024-03-09 21:15:09 +0000

    langchain_test: add RAG

--- langchain_test.R
+++ langchain_test.R
@@ -4,8 +4,18 @@ source("params.R")
 
 llms = import("langchain_community.llms")
 embeddings_import = import("langchain_community.embeddings")
+chains = import("langchain.chains")
 
-llm = llms$Ollama(model = model)
+llm = llms$Ollama(model = model, temperature = 0)
 embeddings = embeddings_import$OllamaEmbeddings(model = model)
+vectordb = Chroma(persist_directory=chromadb_file,
+                  embedding_function=embeddings)
 
-llm$invoke("How much wood would a woodchuck chuck?")
+query = "Which treatment has the least side effects for COPD?"
+sim_docs = vectordb$similarity_search(query)
+
+mm_docs = vectordb$max_marginal_relevance_search(query, k = 3, fetch_k = 5L)
+
+qa_chain = chains$RetrievalQA$from_chain_type(llm,retriever=vectordb$as_retriever())
+
+qa_chain$invoke(query)
