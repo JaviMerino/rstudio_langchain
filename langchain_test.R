d9513e243289895e11449224373bdffe64a7ce81
Javi Merino <merino.jav@gmail.com>
2024-03-14 08:48:17 +0100

    langchain: Make a note about the searching technique

--- langchain_test.R
+++ langchain_test.R
@@ -12,9 +12,11 @@ vectordb = Chroma(persist_directory=chromadb_file,
                   embedding_function=embeddings)
 
 query = "Which treatment has the least side effects for COPD?"
-sim_docs = vectordb$similarity_search(query)
 
-mm_docs = vectordb$max_marginal_relevance_search(query, k = 3, fetch_k = 5L)
+# By default it does:
+#   sim_docs = vectordb$similarity_search(query)
+# But this could be interesting
+#   mm_docs = vectordb$max_marginal_relevance_search(query, k = 4, fetch_k = 10L)
 
 qa_chain = chains$RetrievalQA$from_chain_type(llm,retriever=vectordb$as_retriever())
 
