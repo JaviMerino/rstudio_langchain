eb6901484c6ea7afc3da12ebc6c82bd4e5a4cda8
Javi Merino <merino.jav@gmail.com>
2024-03-14 08:45:58 +0100

    injest_docs: load all the PDFs in the docs/ directory

--- injest_docs.R
+++ injest_docs.R
@@ -7,18 +7,17 @@ document_loaders = import("langchain_community.document_loaders")
 text_splitter = import("langchain.text_splitter")
 embeddings_import = import("langchain_community.embeddings")
 vectorstores = import("langchain_community.vectorstores")
-PyPDFLoader = document_loaders$PyPDFLoader
+PyPDFDirectoryLoader = document_loaders$PyPDFDirectoryLoader
 doc_splitter = text_splitter$RecursiveCharacterTextSplitter()
 embeddings = embeddings_import$OllamaEmbeddings(model=model)
 Chroma = vectorstores$Chroma
 
-# TODO Generalize to all PDFs in the docs folder
-my_doc = PyPDFLoader('docs/ggplot2.pdf')
-doc_pages = my_doc$load()
+my_docs = PyPDFDirectoryLoader('docs/')
+docs_pages = my_docs$load()
 
-doc_splitted = doc_splitter$split_documents(doc_pages)
+docs_splitted = doc_splitter$split_documents(docs_pages)
 vectordb = Chroma$from_documents(
-  documents = doc_splitted,
+  documents = docs_splitted,
   embedding = embeddings,
   persist_directory = chromadb_file
 )
